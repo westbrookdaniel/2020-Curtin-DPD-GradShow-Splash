d7e4474e3ce6b634c82b61acb0cebf9e85e10fb3
westy12dan <westy12dan@gmail.com>
2020-10-19 20:46:14 +0800

    organise

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ const uniforms = {
 }
 
 function init() {
+	colorText()
+
 	const root = document.getElementById('text')
 	material = new Blotter.RollingDistortMaterial()
-    material.uniforms.uSineDistortSpread.value = 0.4
-    material.uniforms.uSineDistortCycleCount.value = 2
-    material.uniforms.uSineDistortAmplitude.value = 0.1
-    material.uniforms.uNoiseDistortVolatility.value = 0
-	
+	material.uniforms.uSineDistortSpread.value = 0.4
+	material.uniforms.uSineDistortCycleCount.value = 2
+	material.uniforms.uSineDistortAmplitude.value = 0.1
+	material.uniforms.uNoiseDistortVolatility.value = 0
+
 	const text = new Blotter.Text(root.innerText, {
 		weight: 800,
 		size: 50,
@@ -42,9 +44,39 @@ function animate() {
 function render() {
 	uniforms.volatility += (deep * 0.5 - uniforms.volatility) * fluid
 	uniforms.speed += (deep * 0.5 - uniforms.speed) * fluid
-	
+
 	// material.uniforms.uVolatility.value = uniforms.volatility
 	// material.uniforms.uSpeed.value = uniforms.speed
+}
+
+function colorText() {
+	const max = 360
+	const colorEl = document.querySelector('.color')
+
+	let hue = 0
+	let inc = true
+	const render = () => {
+		colorEl.style.color = `hsl(${hue}, 63%, 59%)`
+
+		if (hue >= 360) {
+			inc = false
+		} else if (hue <= 0) {
+			inc = true
+		}
+
+		if (inc === true) {
+			hue += 1
+		} else {
+			hue -= 1
+		}
+	}
+
+	const animate = () => {
+		requestAnimationFrame(animate)
+		render()
+	}
+
+	animate()
 }
 
 window.onload = init()
