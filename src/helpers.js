026db60b2f3f0f67b38999c4bf1121c666e91384
westy12dan <westy12dan@gmail.com>
2020-10-20 00:07:51 +0800

    pretty

--- src/helpers.js
+++ src/helpers.js
@@ -1,25 +1,27 @@
-import anime from 'animejs'
+import anime from "animejs";
 
-const trans = 'var(--transition)'
-const ease = 'easeInOutCubic'
-const dur = 500
-const sta = 200
+const trans = "var(--transition)";
+const ease = "easeInOutCubic";
+const dur = 500;
+const sta = 200;
 
 export const animeInit = (selector) => {
-	selector.forEach((sel) => {
-		const el = document.querySelectorAll(sel)
-		el.forEach((el) => {
-			el.style.transition = 'unset'
-			el.style.opacity = '0'
-		})
-	})
-}
+    selector.forEach((sel) => {
+        const el = document.querySelectorAll(sel);
+        el.forEach((el) => {
+            el.style.transition = "unset";
+            el.style.opacity = "0";
+        });
+    });
+};
 
 export const fadeIn = (selector, t = trans) => {
-	return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         try {
-            const el = document.querySelectorAll(selector)
-            if (el.length === 0) { resolve() }
+            const el = document.querySelectorAll(selector);
+            if (el.length === 0) {
+                resolve();
+            }
             anime({
                 targets: el,
                 easing: ease,
@@ -28,13 +30,13 @@ export const fadeIn = (selector, t = trans) => {
                 delay: anime.stagger(sta),
                 complete: () => {
                     el.forEach((el) => {
-                        el.style.transition = t
-                    })
-                    resolve()
+                        el.style.transition = t;
+                    });
+                    resolve();
                 },
-            })
+            });
         } catch (err) {
-            reject(err)
+            reject(err);
         }
-	})
-}
+    });
+};
--- src/index.js
+++ src/index.js
@@ -1,92 +1,92 @@
-import { animeInit, fadeIn } from './helpers'
-import './style.css'
+import { animeInit, fadeIn } from "./helpers";
+import "./style.css";
 
-document.body.style.opacity = 0
+document.body.style.opacity = 0;
 
 function animateText() {
-	let material
-	const deep = 0.5
-	const fluid = 0.015
-	const uniforms = {
-		speed: 0,
-		volatility: 0,
-	}
+    let material;
+    const deep = 0.5;
+    const fluid = 0.015;
+    const uniforms = {
+        speed: 0,
+        volatility: 0,
+    };
 
-	const root = document.getElementById('text')
-	material = new Blotter.RollingDistortMaterial()
-	material.uniforms.uSineDistortSpread.value = 0.4
-	material.uniforms.uSineDistortCycleCount.value = 2
-	material.uniforms.uSineDistortAmplitude.value = 0.1
-	material.uniforms.uNoiseDistortVolatility.value = 0
+    const root = document.getElementById("text");
+    material = new Blotter.RollingDistortMaterial();
+    material.uniforms.uSineDistortSpread.value = 0.4;
+    material.uniforms.uSineDistortCycleCount.value = 2;
+    material.uniforms.uSineDistortAmplitude.value = 0.1;
+    material.uniforms.uNoiseDistortVolatility.value = 0;
 
-	const text = new Blotter.Text(root.innerText, {
-		weight: 800,
-		family: "Helvetica Neue",
-		size: 50,
-		fill: 'white',
-		paddingLeft: 80,
-		paddingRight: 80,
-		paddingBottom: 80,
-		paddingTop: 80,
-	})
-	root.innerText = ''
+    const text = new Blotter.Text(root.innerText, {
+        weight: 800,
+        family: "Helvetica Neue",
+        size: 50,
+        fill: "white",
+        paddingLeft: 80,
+        paddingRight: 80,
+        paddingBottom: 80,
+        paddingTop: 80,
+    });
+    root.innerText = "";
 
-	var blotter = new Blotter(material, {
-		texts: text,
-	})
-	var canvas = blotter.forText(text).domElement
-	root.appendChild(canvas)
+    var blotter = new Blotter(material, {
+        texts: text,
+    });
+    var canvas = blotter.forText(text).domElement;
+    root.appendChild(canvas);
 
-	animate()
+    animate();
 
-	function animate() {
-		requestAnimationFrame(animate)
-		render()
-	}
+    function animate() {
+        requestAnimationFrame(animate);
+        render();
+    }
 
-	function render() {
-		uniforms.volatility += (deep * 0.5 - uniforms.volatility) * fluid
-		uniforms.speed += (deep * 0.5 - uniforms.speed) * fluid
-	}
+    function render() {
+        uniforms.volatility += (deep * 0.5 - uniforms.volatility) * fluid;
+        uniforms.speed += (deep * 0.5 - uniforms.speed) * fluid;
+    }
 }
 
 function colorText() {
-	const max = 360
-	const colorArr = document.querySelectorAll('.color svg path')
-	let hue = 0
-	let inc = true
+    const max = 360;
+    const colorArr = document.querySelectorAll(".color svg path");
+    let hue = 0;
+    let inc = true;
 
-	const render = () => {
-		colorArr.forEach(colorEl => {
-			colorEl.style.fill = `hsl(${hue}, 63%, 59%)`
-		})
+    const render = () => {
+        colorArr.forEach((colorEl) => {
+            colorEl.style.fill = `hsl(${hue}, 63%, 59%)`;
+        });
 
-		if (hue >= 360) {
-			inc = false
-		} else if (hue <= 0) {
-			inc = true
-		}
+        if (hue >= 360) {
+            inc = false;
+        } else if (hue <= 0) {
+            inc = true;
+        }
 
-		if (inc === true) {
-			hue += 1
-		} else {
-			hue -= 1
-		}
-	}
+        if (inc === true) {
+            hue += 1;
+        } else {
+            hue -= 1;
+        }
+    };
 
-	const animate = () => {
-		requestAnimationFrame(animate)
-		render()
-	}
-	animate()
+    const animate = () => {
+        requestAnimationFrame(animate);
+        render();
+    };
+    animate();
 }
 
 window.onload = () => {
-	animeInit(['main', '#text'])
-	animateText()
-	colorText()
-	document.body.style.opacity = 1
-	fadeIn('main').then(() => {
-		fadeIn('#text')
-	})
-}
+    animeInit(["main", "#text"]);
+    animateText();
+    colorText();
+    document.body.style.opacity = 1;
+    fadeIn("main").then(() => {
+        fadeIn("#text");
+    });
+};
